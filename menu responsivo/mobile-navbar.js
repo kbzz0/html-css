ae2acb9f5c051b8698826dbd3d015f40d9a7b5d1
Eduardo Dourado <111649100+kbzz0@users.noreply.github.com>
2023-02-24 20:14:54 -0300

    menu animado

--- menu responsivo/mobile-navbar.js
+++ menu responsivo/mobile-navbar.js
@@ -1,3 +1,25 @@
-class MobileNavBar {
-    constructor(mobileMenu, navList, navLinks)
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if(this.mobileMenu){
+            this.addClickEvent ();
+        }
+        return this;
+    }
 }
+
+const mobileNavbar = new MobileNavbar(
+    ".mobileMenu",
+    ".nav-list",
+    ".nav-list li",
+);
